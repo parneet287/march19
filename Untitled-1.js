8c9d7035d97226dbd6dd5533de8d44a0419fb4b0
Parneet Kaur <742279@student.tor.loc>
2019-03-19 16:06:02 -0400

    another commit

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+      {
+         var para =$("p");
+            for(inti-0;i<para.length;i++)
+             {
+                alert("Paragraph no:" +para[i].innerHTML);
+             }
+         });
+
+   
